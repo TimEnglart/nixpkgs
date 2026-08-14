ba53e15dffd71eb9fd91f592f6cf64bb06a40756
Tim Englart <timenglartdev@gmail.com>
2026-08-14 11:26:24 +1000

    fix yajsw pathing

--- pkgs/tools/security/ghidra/build.nix
+++ pkgs/tools/security/ghidra/build.nix
@@ -186,7 +186,7 @@ stdenv.mkDerivation (finalAttrs: {
       --prefix PATH : ${lib.makeBinPath [ openjdk21 ]}
 
     # Needed by the Ghidra Server NixOS module. Create a symlink in order to provide a stable path.
-    ln -s "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw-stable-13.12" "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw"
+    ln -s "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw-stable-"* "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw"
 
     # Rewrite hard-coded path to Ghidra server config file
     # to what works for the NixOS module.
